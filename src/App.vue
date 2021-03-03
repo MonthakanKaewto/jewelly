6fe76a76bd9e5b32bf0135b21d77ce21a9e5dd90
MonthakanKaewto <75597066+MonthakanKaewto@users.noreply.github.com>
2021-03-03 17:48:39 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,100 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <b-navbar type="dark" variant="dark">
+        <b-navbar-nav>
+          <router-link to="/" class="container">
+            <b-nav-item href="/">TuuShop</b-nav-item>
+          </router-link>
+        </b-navbar-nav>
+        <b-navebar-nav class="ml-auto">
+          <div>
+            <router-link to="/About">
+              <b-button variant="outline-info" @click="sum">
+                <b-icon icon="basket" aria-hidden="ture"></b-icon>
+              </b-button>
+            </router-link>
+          </div>
+        </b-navebar-nav>
+      </b-navbar>
     </div>
-    <router-view/>
+    <router-view
+      :cardList="cardList"
+      :product="product"
+      @update-cart="UpProduct"
+      :tprice="tprice"
+    />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      cardList: [
+        {
+          price: 20000,
+          nameProduct: "รถถัง",
+          img:
+            "https://www.mrlhobby.com/wp-content/uploads/2020/09/tamiya-35055-m41-walker-wip-test-8.jpg"
+        },
+        {
+          price: 10000,
+          nameProduct: "เรือดำน้ำ",
+          img:
+            "https://kapitaennem0.files.wordpress.com/2017/05/c0151c5f0893b3bfeb4eda2deb3a7222.jpg?w=584"
+        },
+        {
+          price: 25000,
+          nameProduct: "เฮลิคอปเตอร์",
+          img:
+            "https://cdn.pixabay.com/photo/2021/01/24/21/58/helicopter-5946667__340.png"
+        },
+        {
+          price: 9500,
+          nameProduct: "โล่ตำรวจ",
+          img:
+            "https://www.grossiste-chinois-import.com/images/full_size/bouclier_police_POLBCLKL01.jpg"
+        },
+        {
+          price: 2000,
+          nameProduct: "กระบองตำรวจ",
+          img: "https://www.newtv.co.th/images/content/ct_20190702111959469.jpg"
+        },
+        {
+          price: 5000,
+          nameProduct: "แก๊สน้ําตา",
+          img: "https://www.catdumb.com/wp-content/uploads/2019/05/2-389.jpg"
+        },
+        {
+          price: 200000,
+          nameProduct: "M4A1",
+          img:
+            "https://cdn.shopify.com/s/files/1/0546/8541/products/TIPPMANN_M4_CARBINE_AIRSOFT_RIFLE1_clipped_rev_ROJ17E83UAKZ_2000x.png?v=1575931652"
+        },
+        {
+          price: 2000,
+          nameProduct: "รองเท้าคอมแบท",
+          img: "https://dy.lnwfile.com/x7f4nw.jpg"
+        }
+      ],
+      product: [],
+      tprice: 0
+    };
+  },
+  methods: {
+    UpProduct(value) {
+      this.product.push(value);
+    },
+    sum() {
+      this.tprice = 0;
+      for (var i = 0; i < this.product.length; i++) {
+        this.tprice = this.tprice + this.product[i].Sum;
+      }
+    }
+  }
+};
+</script>
 
 <style>
 #app {
@@ -16,16 +104,13 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <br />
+      <b-row>
+        <b-col cols="lg-4" v-for="index in cardList" :key="index">
+          <Product
+            :img="index.img"
+            :price="index.price"
+            :nameProduct="index.nameProduct"
+            @update-cart="sub"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-modal id="bv-modal-example" hide-footer>
+      <template #modal-title class="text-center">แจ้งเตือน!!</template>
+      <div class="d-block text-center">
+        <p>หยิบสินค้าเรียบร้อย!!</p>
+      </div>
+      <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example')"
+        >ปิด</b-button
+      >
+    </b-modal>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Product from "@/components/product.vue";
 export default {
-  name: 'Home',
+  name: "Home",
+  props: {
+    cardList: Array
+  },
   components: {
-    HelloWorld
+    Product
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    sub(value) {
+      this.$emit("update-cart", value);
+    }
   }
-}
+};
 </script>
